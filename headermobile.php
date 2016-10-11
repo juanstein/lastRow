9d220696d00e4d0253b5800fa31e4f249043c854
imansilladerqui <imansilladerqui@hotmail.com>
2016-10-11 19:56:40 -0300

    Add files via upload

---
+++ headermobile.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header uppercase">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.html">Marca</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="host"><a href="host.php" class="hostmobile">¿Querés Hostear?</a><a class="hostdesktop">¿Querés Hostear?</a></li>
+        <li class="openhelp"><a href="ayuda.php" class="ayudamobile">Ayuda</a><a class="ayudadesktop">Ayuda</a></li>
+        <li class="openregs"><a href="registracion.php" class="registracionmobile">Registración</a><a class="registraciondesktop">Registración</a></li>
+        <li class="opensession"><a href="sesion.php" class="sesionmobile">Sesión</a><a class="sesiondesktop">Sesión</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
--- js/main.js
+++ js/main.js
@@ -1,23 +1,39 @@
 $(document).ready(function() {
 
-$(".openhelp").click(function(){
-        $("#modalhelp").modal({backdrop: true});
-        $(".modal-backdrop").css("background-color", "transparent");
+	$(".openhelp").click(function(){
+	  $("#modalhelp").modal({backdrop: true});
+	  $(".modal-backdrop").css("background-color", "transparent");
+	});
+
+	$('.openregs').on('click', function (e) {
+	  $('#modalregs').modal('show');
+	});
+
+	$('.opensession').on('click', function (e) {
+	  $('#modalsession').modal('show');
+	});
+
+	$("li.questionhelp").click(function() {
+		$(this).find('i').toggleClass('fa-angle-down fa-angle-up');
+		$(this).children('ul').toggle();
+	});
 });
 
-$('.openregs').on('click', function (e) {
-  $('#modalregs').modal('show');
-});
+// Get the modal
+var modal = document.getElementById('modalhost');
 
-$('.opensession').on('click', function (e) {
-  $('#modalsession').modal('show');
-});
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-	
-$("li.questionhelp").click(function() {
-	$(this).find('i').toggleClass('fa-angle-down fa-angle-up');
-	$(this).children('ul').toggle();
-});
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
-});
